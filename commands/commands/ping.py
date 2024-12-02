6a8fd1fe783236cb5e696d9c61499e683cfad042
Brandon N <71760080+toxieon@users.noreply.github.com>
2024-12-02 21:29:04 +1100

    Delete commands directory

--- commands/commands/ping.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-# Define the Ping command
-async def setup(bot):
-    @bot.command(name="Ping")
-    async def ping(ctx):
-        """Simple ping command to check if the bot is online."""
-        await ctx.send("ToxiBot Online")
